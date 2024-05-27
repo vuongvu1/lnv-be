405c8d4e447d4276ef52296900a5e427f579df7e
Vuong Vu <hoangvuong.v99@gmail.com>
2024-05-27 17:47:33 +0200

    chore: Update API endpoint to use "q" query parameter for asking questions

--- api/index.js
+++ api/index.js
@@ -5,6 +5,22 @@ const openai = new OpenAI();
 const app = express();
 const port = 3001;
 
+const query =
+  "Create a conversation between two random individuals in an unspecified location, discussing arbitrary topics with a late-night ambiance. Ensure the conversation can loop infinitely with a smooth transition from the end back to the start. Include their names and format it as follows: '{personOne: string, personTwo: string, conversation: [{person: string, content: string}]}'.";
+
+const storyOfTheDay = {};
+
+function getCurrentDate() {
+  const today = new Date();
+
+  const day = today.getDate();
+  const month = today.getMonth() + 1; // Months are zero-based, so we add 1
+  const year = today.getFullYear();
+
+  const currentDate = `${day}-${month}-${year}`;
+  return currentDate;
+}
+
 async function askGPT(message) {
   const completion = await openai.chat.completions.create({
     messages: [{ role: "system", content: message }],
@@ -14,10 +30,16 @@ async function askGPT(message) {
   return completion.choices[0].message.content;
 }
 
-app.get("/ask", async (req, res) => {
-  const { q: question } = req.query;
-  const reply = question ? await askGPT(question) : "No question asked.";
-  res.send(reply);
+app.get("/story", async (req, res) => {
+  const today = getCurrentDate();
+  if (storyOfTheDay[today]) {
+    return res.send(storyOfTheDay[today]);
+  }
+
+  const story = await askGPT(query);
+  storyOfTheDay[today] = story;
+
+  res.send(story);
 });
 
 app.listen(port, () => {
